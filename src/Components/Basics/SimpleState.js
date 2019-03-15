df053369ce908da664ba3e77bc91262d6aebf7d3
matovumansoor70 <mansoormatovu70@gmail.com>
2019-03-15 13:59:47 +0300

    my react

---
+++ src/Components/Basics/SimpleState.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class SimpleState extends Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: "hello",
+      world: "World",
+      hands: 2,
+      legs: 2
+    };
+    this.name = "Anura";
+    this.age = 30;
+    this.changeState = this.changeState.bind(this);
+    this.changeStateWorld = this.changeStateWorld.bind(this);
+    this.t= this.changeStateHand.bind(this);
+  }
+  changeState() {
+    if (this.state.world == "World") {
+      this.setState({ greeting: "Habari" });
+    } else if (this.state.world == "Earth") {
+      this.setState({ greeting: "This is Kenya" });
+    }
+  }
+  changeStateWorld() {
+    this.setState({ world: "Earth" });
+  }
+
+  componentDidMount() {
+    this.changeState();
+  }
+
+  changeStateHand =()=> {
+    this.setState({ hands: 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.hands}</h1>
+        <h1>
+          {this.state.greeting} {this.state.world}
+        </h1>
+        <button onClick={this.changeState}>Click me to change greeting</button>
+        <button onClick={this.changeStateWorld}>
+          Click me to change world
+        </button>
+        <h3>{this.name}</h3>
+        <h3>{this.age}</h3>
+
+        <button onClick={this.changeStateHand}>hands button</button>
+      </div>
+    );
+  }
+}
+
+export default SimpleState;
---
+++ src/Components/Layout/Navbar.js
@@ -0,0 +1,17 @@
+import React, { Component, Fragment } from 'react';
+
+class Navbar extends Component {
+render(){
+return(
+<Fragment>
+<nav className="navbar navbar-light bg-secondary fixed-top" >
+<a className="navbar-brand"> Laces an Buttons</a>
+<a clasName="nav-link"> About</a>
+</nav>
+
+
+</Fragment>
+);
+}
+}
+export default Navbar;
